913124cc60f943e766a7c56c64e74619246cc439
ISoLuNA <ISoLuNA@ASUS>
2019-06-20 09:59:27 +0800

    Background Design

---
+++ cache/66/5b636f6e766572736174696f6e2d363132616233383833353662386138626161333564393961353365613532336431333262393863622d363132616233383833353662386138626161333564393961353365613532336431333262393863625d5b315d.doctrinecache.php
@@ -0,0 +1,5 @@
+<?php return unserialize('a:2:{s:8:"lifetime";i:1560997723;s:4:"data";a:5:{s:12:"conversation";O:19:"inquireConversation":3:{s:7:"' . "\0" . '*' . "\0" . 'name";N;s:8:"' . "\0" . '*' . "\0" . 'token";N;s:12:"' . "\0" . '*' . "\0" . 'cacheTime";N;}s:8:"question";s:44:"s:36:"Good day! May I know your full name?";";s:20:"additionalParameters";s:6:"a:0:{}";s:4:"next";s:451:"C:32:"Opis\\Closure\\SerializableClosure":405:{a:5:{s:3:"use";a:0:{}s:8:"function";s:172:"function(\\BotMan\\BotMan\\Messages\\Incoming\\Answer $answer) {
+            // Save result
+            $this->name = $answer->getText();
+            $this->welcome();
+        }";s:5:"scope";s:19:"inquireConversation";s:4:"this";O:19:"inquireConversation":3:{s:7:"' . "\0" . '*' . "\0" . 'name";N;s:8:"' . "\0" . '*' . "\0" . 'token";N;s:12:"' . "\0" . '*' . "\0" . 'cacheTime";N;}s:4:"self";s:32:"00000000659cb268000000000168fcdb";}}";s:4:"time";s:21:"0.95133600 1560995923";}}');
